763ede5c2b15b036bb0b809c9b5892bdc098102c
Student User <student@workstation.lab.example.com>
2021-11-08 05:54:00 -0500

    Update

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.listen(8080, function() {
   console.log('listening on 8080')
 })
 
-app.use(healthz);
+app.use(healthz)
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
