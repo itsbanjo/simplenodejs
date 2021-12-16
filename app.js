0031f56c49b8675e118dd05b3938dfa564e7a9e4
Mariel Awat Donila <mdonila@mdonila.remote.csb>
2021-12-17 10:30:51 +1300

    update

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const expressHealth = require('express-health-middleware');
 app.use(expressHealth());
+app.use(bodyParser.json());
 
 
 const env = process.env;
@@ -23,7 +24,7 @@ app.use('/', (req, res, next) => {
   res.render('index.pug', { name: env.NAME , age: env.AGE });
 });
 
-app.use(bodyParser.json())
+
 app.post("/hook", (req, res) => {
   console.log(req.body) // Call your action on the request here
   res.status(200).end() // Responding is important
